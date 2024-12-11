02aae605f85052fba33d75adc425cc8e49fe1427
Sayidbek Akmalov <sbk.akmalov@gmail.com>
2024-12-11 10:56:43 +0500

    Small changes for meta env logic

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,4 +1,13 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  VITE_API_URL: string;
+}
+
+interface ImportMeta {
+  env: ImportMetaEnv;
+}
+
 declare module "*.png";
 declare module "*.svg";
 declare module "*.jpeg";
